1c8fe2fa76a8bc6b6e98f6ef60adf28fe7b038fa
Prrremydraya <git.user.>
2025-08-12 20:13:13 +0300

    Main jar for Enter proj

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Введите текст и нажмите <Enter> :");
+        System.out.println("Введите текст и нажмите <Enter>:");
         String text = new Scanner(System.in).nextLine();
         System.out.println("Длина текста:" + text.length());
 
