948fe0a06f1e4fc305e573f49955bb138638ec6d
Prrremudraya <git.user.>
2025-08-14 01:58:49 +0300

    Course project. Assignment on the topic "Language syntax and basic data types"

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner console = new Scanner(System.in);
-        System.out.println("Введите число:");
+        System.out.println("Введите первое число:");
         int number = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число:");
         int numbertwo = new Scanner(System.in).nextInt();
 
-        int firstNumber= 5;
-        int secondNumber= 10;
+        int firstNumber= number;
+        int secondNumber= numbertwo;
         double quotient = (double) firstNumber/secondNumber;
         System.out.println("Сумма:" + firstNumber+secondNumber);
         System.out.println("Разность:"+ (firstNumber-secondNumber));
