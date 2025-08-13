f61914114b14bd3b561d6596251fa200d1af5ce9
Prrremudraya <git.user.>
2025-08-14 01:42:18 +0300

    Course Operators homework

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,11 +5,17 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = new Scanner(System.in).nextInt();
+        int numbertwo = new Scanner(System.in).nextInt();
 
-        System.out.println("Введите текст и нажмите <Enter> :");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста:" + text.length());
-
+        int firstNumber= 5;
+        int secondNumber= 10;
+        double quotient = (double) firstNumber/secondNumber;
+        System.out.println("Сумма:" + firstNumber+secondNumber);
+        System.out.println("Разность:"+ (firstNumber-secondNumber));
+        System.out.println("Произведение:" + firstNumber*secondNumber);
+        System.out.println("Частное:" + quotient);
     }
-
 }
